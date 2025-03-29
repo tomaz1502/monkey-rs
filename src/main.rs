d73326a8a5b819a6b3fa8fbc8f332d427fe40d33
Tomaz <tomgm1502@gmail.com>
2025-03-29 16:45:04 -0300

    Add repl

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::fs;
+use std::io::Write;
 use std::path::Path;
 use std::env;
 use std::io;
+use std::io::BufRead;
 use std::process;
 
-use mods::lexer::lexer;
+use mods::parser::lexer;
 
 mod mods;
 
@@ -24,15 +26,38 @@ fn get_text(file_path: &str) -> io::Result<String>
     }
 }
 
+fn repl() -> io::Result<()>
+{
+    let stdin = io::stdin();
+    loop {
+        print!(">> ");
+        io::stdout().flush()?;
+        let line = stdin.lock().lines().next().unwrap().unwrap();
+        let mut tokenizer = lexer::Tokenizer::new(line);
+        loop {
+            let tkn = tokenizer.get_next();
+            match tkn {
+                Ok(tkn) => println!("{:?}", tkn),
+                Err(lexer::TknError::Eof) => break,
+                Err(e) => panic!("unexpected error: {:?}", e)
+            }
+        }
+    }
+}
+
 fn main() -> io::Result<()>
 {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        die("Usage: monkey <program path>");
+    if args.len() > 2 {
+        die("Usage: monkey <program path> or just monkey");
     }
-    let text = get_text(&args[1])?;
-    let mut tokenizer = lexer::Tokenizer::new(text);
-    let _tkn = tokenizer.get_next();
+    if args.len() == 2 {
+        let text = get_text(&args[1])?;
+        let mut tokenizer = lexer::Tokenizer::new(text);
+        let _tkn = tokenizer.get_next();
+    } else {
+        repl()?;
+    }
 
     Ok(())
 }
--- src/mods/parser/lexer.rs
+++ src/mods/parser/lexer.rs
@@ -36,7 +36,7 @@ pub enum Token {
     False,
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 pub enum TknError { UnrecognizedToken, Eof }
 
 use Token::*;
