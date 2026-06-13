32b3f0fe20aa18677f6e11500d71739b561ebcf9
Tomaz Gomes Mascarenhas <tomgm1502@gmail.com>
2026-06-13 17:34:43 -0300

    Change type of main

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::env;
 use std::fs;
 use std::io;
 use std::path::Path;
-use std::process;
 use io::{Write, BufRead};
 
 use mods::lib::parser;
@@ -13,42 +12,41 @@ use mods::lib::evaluator::Evaluate;
 
 mod mods;
 
-fn die(msg: &str) {
-    println!("{}", msg);
-    process::exit(1)
-}
-
-fn get_text(file_path: &str) -> io::Result<String> {
+fn get_text(file_path: &str) -> Result<String, String> {
     if Path::new(file_path).exists() {
-        fs::read_to_string(file_path)
+        fs::read_to_string(file_path).map_err(|e| e.to_string())
     } else {
         let err_msg = std::format!("File does not exist: {}", file_path);
-        Err(io::Error::other(err_msg))
+        Err(err_msg)
     }
 }
 
-fn process(text: String, tc_ctx: &mut type_checker::Context, ev_ctx: &mut evaluator::Context) -> io::Result<()> {
+fn process(text: String,
+           tc_ctx: &mut type_checker::Context,
+           ev_ctx: &mut evaluator::Context)
+           -> Result<(), String> {
     let prog = parser::Parser::parse(text)?;
+    println!("prog = {prog:?}");
     for stmt in prog.stmts {
-        tc_ctx.tc(&stmt).ok_or(std::io::Error::other("Typing error."))?;
-        ev_ctx.eval(&stmt).ok_or(std::io::Error::other("Evaluation error."))?;
+        tc_ctx.tc(&stmt).ok_or("Typing error.".to_string())?;
+        ev_ctx.eval(&stmt).ok_or("Evaluation error.".to_string())?;
     }
     Ok(())
 }
 
-fn repl() -> io::Result<()> {
+fn repl() -> Result<(), String> {
     let stdin = io::stdin();
     let mut tc_ctx = type_checker::Context::new();
     let mut ev_ctx = evaluator::Context::new();
     loop {
         print!(">> ");
-        io::stdout().flush()?;
+        io::stdout().flush().map_err(|e| e.to_string())?;
         let line = stdin.lock().lines().next().unwrap().unwrap();
         process(line, &mut tc_ctx, &mut ev_ctx)?;
     }
 }
 
-fn interpret_file(file_path: &str) -> io::Result<()> {
+fn interpret_file(file_path: &str) -> Result<(), String> {
     let source_code = get_text(file_path)?;
     let mut tc_ctx = type_checker::Context::new();
     let mut ev_ctx = evaluator::Context::new();
@@ -56,10 +54,10 @@ fn interpret_file(file_path: &str) -> io::Result<()> {
     Ok(())
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
     if args.len() > 2 {
-        die("Usage: monkey <program path> or just monkey");
+        return Err("Usage: monkey <program_path>?".to_string());
     } else if args.len() == 2 {
         interpret_file(&args[1])?;
     } else {
